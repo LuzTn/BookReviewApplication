9b68cc34bd9ea0a060b52b10e5974dd963008d00
Nour <nour.sboui1993@gmail.com>
2018-12-04 10:53:57 +0100

    Book State new entity

---
+++ src/main/java/bookReview/model/BookUserState.java
@@ -0,0 +1,40 @@
+package bookReview.model;
+
+import javax.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+@Table(name = "bookUserState")
+public class BookUserState {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Book book;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+}
